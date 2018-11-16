9072b357db4697c16dad9861dbaa84f8b0e164bb
Leandro Jesus <Leandro Jesus>
2018-11-16 14:45:57 +0000

    QUICKFIX MainMenuController implements, again, Controller Interface | Added controllers property to navigation bean and added the MainMenuController entry

--- src/main/java/com/ljprogramming/app/controller/MainMenuController.java
+++ src/main/java/com/ljprogramming/app/controller/MainMenuController.java
@@ -5,7 +5,7 @@ import javafx.fxml.FXML;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 
-public class MainMenuController {
+public class MainMenuController implements Controller{
 
     private Navigation navigation;
 
