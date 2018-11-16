f9b7f2df8730a169d73ebbe87abe4e4d2674a979
Leandro Jesus <Leandro Jesus>
2018-11-16 13:32:58 +0000

    Added UI elements to MainMenuController and the corresponding events

--- src/main/java/com/ljprogramming/app/controller/MainMenuController.java
+++ src/main/java/com/ljprogramming/app/controller/MainMenuController.java
@@ -1,4 +1,36 @@
 package com.ljprogramming.app.controller;
 
-public class MainMenuController implements Controller{
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+
+public class MainMenuController {
+
+    @FXML
+    private ImageView imgTitle;
+
+    @FXML
+    private ImageView imgPlay;
+
+    @FXML
+    private ImageView imgOptions;
+
+    @FXML
+    private ImageView imgExit;
+
+    @FXML
+    void onPlayClick(MouseEvent event) {
+
+    }
+
+    @FXML
+    void onOptionsClick(MouseEvent event) {
+
+    }
+
+    @FXML
+    void onExitClick(MouseEvent event) {
+
+    }
+
 }
