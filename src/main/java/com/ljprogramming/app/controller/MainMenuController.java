ef622dc80ce89afa1be17fd62115b88d81aa8ed7
Leandro Jesus <Leandro Jesus>
2018-11-16 18:42:25 +0000

    Added  mouse Hover and mouse Exit events to MainMenuController

--- src/main/java/com/ljprogramming/app/controller/MainMenuController.java
+++ src/main/java/com/ljprogramming/app/controller/MainMenuController.java
@@ -4,6 +4,8 @@ import com.ljprogramming.app.Navigation;
 import javafx.fxml.FXML;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.shape.Polygon;
+import javafx.scene.shape.Shape;
 
 public class MainMenuController implements Controller{
 
@@ -21,8 +23,19 @@ public class MainMenuController implements Controller{
     @FXML
     private ImageView imgExit;
 
+
     @FXML
     void onPlayClick(MouseEvent event) {
+
+    }
+
+    @FXML
+    void onPlayMouseExit(MouseEvent event) {
+
+    }
+
+    @FXML
+    void onPlayMouseHover(MouseEvent event) {
 
     }
 
@@ -32,8 +45,28 @@ public class MainMenuController implements Controller{
     }
 
     @FXML
+    void onOptionsMouseExit(MouseEvent event) {
+
+    }
+
+    @FXML
+    void onOptionsMouseHover(MouseEvent event) {
+
+    }
+
+    @FXML
     void onExitClick(MouseEvent event) {
-        System.exit(0);
+
+    }
+
+    @FXML
+    void onExitMouseExit(MouseEvent event) {
+
+    }
+
+    @FXML
+    void onExitMouseHover(MouseEvent event) {
+
     }
 
     public void setNavigation(Navigation navigation) {
