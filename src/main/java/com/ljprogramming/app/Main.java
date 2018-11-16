52769ca5140fae23f899aba918d0f7a8b461e155
Leandro Jesus <Leandro Jesus>
2018-11-16 16:15:20 +0000

    QUICKFIX changed the Navigation singleton setStage Acess Modifier from Private to Public

--- src/main/java/com/ljprogramming/app/Main.java
+++ src/main/java/com/ljprogramming/app/Main.java
@@ -2,8 +2,13 @@ package com.ljprogramming.app;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main extends Application {
+
+    private final String SPRING_PATH = "/spring";
+    private final String SPRING_CONFIG_NAME = "spring-config";
 
     public static void main(String[] args) {
 
@@ -11,6 +16,13 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+        ApplicationContext applicationContext =
+                new ClassPathXmlApplicationContext(SPRING_PATH + "/" + SPRING_PATH + ".xml");
+
+        Navigation navigation = applicationContext.getBean("navigation", Navigation.class);
+
+        navigation.
+
 
     }
 }
--- src/main/java/com/ljprogramming/app/Navigation.java
+++ src/main/java/com/ljprogramming/app/Navigation.java
@@ -66,7 +66,7 @@ public final class Navigation {
         stage.show();
     }
 
-    private void setStage(Stage stage) {
+    public void setStage(Stage stage) {
         this.stage = stage;
     }
 
