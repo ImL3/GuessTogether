e40de83933e6234ba4f025f75f8fa579701bfced
Leandro Jesus <leandrojesus.programmer@gmail.com>
2018-11-18 18:33:35 +0000

    Added getShapeImage method signature to ShapeService Interface

--- src/main/java/com/ljprogramming/app/util/enums/Shape.java
+++ src/main/java/com/ljprogramming/app/util/enums/Shape.java
@@ -8,10 +8,6 @@ public enum Shape {
     POLYGON("Polygon"),
     CIRCLE("Circle");
 
-    public static final String IMG_SHAPES_PATH = "img/mainmenu/shapes";
-    public static final String IMG_EXTENSION = "png";
-    public static final String IMG_NAME_SUFFIX = "Shape";
-
     private String shapeName;
 
     Shape(String shapeName){
